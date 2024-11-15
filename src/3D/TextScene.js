5793b4f8f93f150c487611e637ac3c363380eb36
lantianqi <50139994+lantianqi@users.noreply.github.com>
2024-11-15 23:42:01 +1100

    add env preset and blur control

--- src/3D/TextScene.js
+++ src/3D/TextScene.js
@@ -3,19 +3,9 @@ import { useState, useTransition, useMemo } from 'react'
 import { Canvas } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight, Center, Environment, OrbitControls } from '@react-three/drei'
 
-// import { SketchPicker } from 'react-color';
-
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-
-
-import GlassBox from "./components/GlassBox";
-import ColorPicker from '../ui-components/ColorPicker';
 import GlassText from './components/GlassText';
-import { MuiColorInput } from 'mui-color-input';
-import { Pages } from '@mui/icons-material';
-import { Paper } from '@mui/material';
+import { OutlinedInput, Input, Select, MenuItem, Slider } from '@mui/material';
+
 
 // function Env() {
 //   // You can use the "inTransition" boolean to react to the loading in-between state,
@@ -28,24 +18,82 @@ import { Paper } from '@mui/material';
 function TextScene({ ...props }) {
   const [envPreset, setEnvPreset] = useState('sunset');
   const [boxColor, setBoxColor] = useState('#227142');
+  const [envBlur, setEnvBlur] = useState(0);
 
-  const handleChange = (color) => {
-    setBoxColor(color);
+  const handleColorChange = (event) => {
+    setBoxColor(event.target.value);
   };
 
-  const bgList = ['sunset', 'dawn', 'night', 'warehouse', 'forest', 'apartment', 'studio', 'city', 'park', 'lobby'];
+  const handlePresetChange = (event) => {
+    setEnvPreset(event.target.value);
+  };
+
+  const handleBlurChange = (event) => {
+    setEnvBlur(event.target.value);
+  }
+
+  const optionsList = [
+    { id: 0, value: 'sunset' },
+    { id: 1, value: 'dawn' },
+    { id: 2, value: 'night' },
+    { id: 3, value: 'warehouse' },
+    { id: 4, value: 'forest' },
+    { id: 5, value: 'apartment' },
+    { id: 6, value: 'studio' },
+    { id: 7, value: 'city' },
+    { id: 8, value: 'park' },
+    { id: 9, value: 'lobby' }
+  ];
 
   return (
     <div className='BoxScene'>
-      <div className='ColorInput'>
-        <MuiColorInput
+      <div className='ColorInputContainer'>
+        {/* <MuiColorInput
           value={boxColor}
           format={'hex'}
-          onChange={handleChange}
+          onChange={handleColorChange}
           adornmentPosition='start'
           disablePopover={false}
           isAlphaHidden={true}
+          disableGutters
+        /> */}
+        <Input id='ColorInput'
+          type='color'
+          value={boxColor}
+          format={'hex'}
+          onChange={handleColorChange}
+
         />
+      </div>
+      <div className='BlurInput'>
+        <Slider
+          value={envBlur}
+          onChange={handleBlurChange}
+          min={0}
+          max={1}
+          step={0.001}
+        />
+      </div>
+      <div className='PresetInput'>
+        <Select
+          type='select'
+          labelId="demo-simple-select-standard-label"
+          id="demo-select"
+          value={envPreset}
+          onChange={handlePresetChange}
+          label="Select an Option"
+          input={<OutlinedInput label="Select an Option" />}
+        >
+          {optionsList.length > 0 ? (
+            optionsList.map((option) => (
+              <MenuItem key={option.id} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))
+          ) : (
+            <MenuItem disabled>No options available</MenuItem>
+          )}
+        </Select>
       </div>
 
       <div className='SceneCanvas'>
@@ -64,7 +112,7 @@ function TextScene({ ...props }) {
           <AccumulativeShadows temporal frames={200} color="purple" colorBlend={0.5} opacity={1} scale={10} alphaTest={0.85}>
             <RandomizedLight amount={8} radius={5} ambient={0.5} position={[5, 3, 2]} bias={0.001} />
           </AccumulativeShadows>
-          <Environment preset={envPreset} background backgroundBlurriness={0} />
+          <Environment preset={envPreset} background backgroundBlurriness={envBlur} />
         </Canvas>
       </div>
     </div>
